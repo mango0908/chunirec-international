9179385795d452605c153a93bea81ec7c409a6f8
MANGO <mangomeant@gmail.com>
2024-10-22 14:11:44 +0900

    Update install-chuni-tools.user.js

--- install-chuni-tools.user.js
+++ install-chuni-tools.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         run chuni-tools on all chunithm-net pages
+// @name         run chunirec-international on all chunithm-net pages
 // @version      0.1
-// @description  run chuni-tools on all chunithm-net pages
-// @author       evnchn
+// @description  run chunirec-international on all chunithm-net pages
+// @author       mango0908
 // @match        https://chunithm-net-eng.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
@@ -11,6 +11,6 @@
 (function () {
     'use strict';
     const s = document.createElement('script');
-    s.src = 'https://dogeon188.github.io/chuni-tools/scripts/chuni-tools.js?t=' + Math.floor(Date.now() / 60000);
+    s.src = 'https://mango0908.github.io/chunirec-international/scripts/chunirec-international.js?t=' + Math.floor(Date.now() / 60000);
     document.body.append(s);
 })();
